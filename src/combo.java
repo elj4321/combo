17ad534fa69fae4285d431c304a2a3993bc4d1b5
sb <sb@sb>
2015-02-02 15:39:40 -0600

    works

--- src/combo.java
+++ src/combo.java
@@ -12,9 +12,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.io.PrintStream;
 //import java.net.URL;
 //import java.io.BufferedReader;
@@ -22,7 +24,7 @@ import java.io.PrintStream;
 
 public class combo {
 
-  private static boolean debug = true;
+  private static boolean debug = false;
   private static final String task = "combo";
   private static PrintStream outs = System.out;
 
@@ -39,13 +41,45 @@ public class combo {
     Scanner scanr = new Scanner(new File(infile));
 
     // Read in stuff
-    int s = scanr.nextInt();
-    outd("s = " + s);
+    int N = scanr.nextInt();
+    // If only 1 possible adjust limits, otherwise wrapping does not work
+    int M = 2;
+    if (N == 1) M = 1;
+    outd("N,M = " + N + " " + M);
+    int f1 = scanr.nextInt();
+    int f2 = scanr.nextInt();
+    int f3 = scanr.nextInt();
+    int m1 = scanr.nextInt();
+    int m2 = scanr.nextInt();
+    int m3 = scanr.nextInt();
+    outd("farm: " + f1 + " " + f2 + " " + f3);
+    outd("master: " + m1 + " " + m2 + " " + m3);
 
+    // Build up all possible combinations
+    Set<String> allSet = new HashSet<String>();
+    for (int i = -M; i <= M; i++)
+    {
+    for (int j = -M; j <= M; j++)
+    {
+    for (int k = -M; k <= M; k++)
+    {
+      int fi = (f1 + i); if (fi < 1) fi = fi + N; else if (fi > N) fi = fi - N;
+      int fj = (f2 + j); if (fj < 1) fj = fj + N; else if (fj > N) fj = fj - N;
+      int fk = (f3 + k); if (fk < 1) fk = fk + N; else if (fk > N) fk = fk - N;
+      String newCombo = "" + fi + " " + fj + " " + fk;
+      allSet.add(newCombo);
+      int mi = (m1 + i); if (mi < 1) mi = mi + N; else if (mi > N) mi = mi - N;
+      int mj = (m2 + j); if (mj < 1) mj = mj + N; else if (mj > N) mj = mj - N;
+      int mk = (m3 + k); if (mk < 1) mk = mk + N; else if (mk > N) mk = mk - N;
+      newCombo = "" + mi + " " + mj + " " + mk;
+      allSet.add(newCombo);
+    }
+    }
+    }
 
     // Output results
-    outd("r = ");
-    out.println("1");
+    outd("Total = " + allSet.size());
+    out.println(allSet.size());
     scanr.close();
     out.close();
     System.exit(0);
